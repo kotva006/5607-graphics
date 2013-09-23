3fcf473f4593e85423c00df1f7e4235ae9b98a20
Ryan Kotval <kotva006@umn.edu>
2013-09-23 12:49:01 -0500

    Ray tracer produces an image, although not correctly

--- ray_cast/main.cpp
+++ ray_cast/main.cpp
@@ -37,23 +37,32 @@ int main (int argc, char *argv[]) {
   int counter = 0;
   float *winpos, *raydir;
   string data = "";
+  float mc = 255;
 
   cout << "starting loop\n";
 
+  float test[3] = {1,0,0};
+  float test2[3] = {0,1,0};
+  float *res = vec::crossproduct(test,test2);
+  cout << "C: " << res[0] << " " << res[1] << " " << res[2] << "\n";
+  cout << "Color: " << scene->object[0]->materialcolor[0] << "\n";
+
   for ( i=0; i<scene->pixheight; i++) {
-    for ( j=0; j<scene->pixheight; j++) {
+    for ( j=0; j<scene->pixwidth; j++) {
       winpos = vec::add(w->ul, vec::add(
-               vec::mul(i,w->deltah), vec::mul(j,w->deltav)));
+               vec::mul(j,w->deltah), vec::mul(i,w->deltav)));
       raydir = vec::normalize(vec::sub(winpos,scene->eye)); 
+      //cout << "H: " << w->deltah[1] << " W: " << w->deltav[1] << "\n";
       for (k=0; k<scene->object.size(); k++) {
         float d = (float)pow((double)scene->object[k]->B(scene->eye,raydir),2)-
                   (4*scene->object[k]->C(scene->eye,raydir));
         char color[13];
+        //if (true) {cout << "D: " << (4*scene->object[k]->C(scene->eye,raydir)) << "\n";}
         if(d >= 0) {
-          sprintf(color, "%d %d %d ", (int)scene->object[k]->materialcolor[0],
-                                      (int)scene->object[k]->materialcolor[1],
-                                      (int)scene->object[k]->materialcolor[2]);
-          cout << "Hit";
+          sprintf(color, "%d %d %d ", (int)(scene->object[k]->materialcolor[0] * mc),
+                                      (int)(scene->object[k]->materialcolor[1] * mc),
+                                      (int)(scene->object[k]->materialcolor[2] * mc));
+          //cout << "Hit";
         } else {
           sprintf(color, "%d %d %d ", (int)scene->bkgcolor[0],
                                       (int)scene->bkgcolor[1],
--- ray_cast/vectors.cpp
+++ ray_cast/vectors.cpp
@@ -45,8 +45,8 @@ namespace vec {
 
   float* crossproduct(float* v1,float* v2) {
     float *rvec = (float*) malloc(sizeof(float) * 3);
-    rvec[0] = (v1[1] * v2[2]) - (v2[1] * v1[2]);
-    rvec[1] = (v1[0] * v2[2]) + (v2[0] * v1[2]);
+    rvec[0] = (v1[1] * v2[2]) - (v1[2] * v2[1]);
+    rvec[1] = (v1[2] * v2[0]) - (v1[0] * v2[2]);
     rvec[2] = (v1[0] * v2[1]) - (v1[1] * v2[0]);
     return rvec;
   } 
